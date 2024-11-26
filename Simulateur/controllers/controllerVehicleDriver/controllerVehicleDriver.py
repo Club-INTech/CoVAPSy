995a071789829d0bf45f592c5346a87f906b3d1c
matthiasbienvenu <matthiasbienvenu@outlook.fr>
2024-11-26 23:10:23 +0100

    Replaced fields by emitters, receivers and secured it with locks

--- Simulateur/controllers/controllerVehicleDriver/controllerVehicleDriver.py
+++ Simulateur/controllers/controllerVehicleDriver/controllerVehicleDriver.py
@@ -22,15 +22,15 @@ class VehicleDriver(Driver):
         basicTimeStep = int(self.getBasicTimeStep())
         self.sensorTime = basicTimeStep // 4
 
-        # this is needed if we want to communicate through node level fields
         print("self.getName(): ", self.getName())
+        self.i = int(self.getName().split("_")[-1])
 
-        #Lidar
+        # Lidar
         self.lidar = self.getDevice("RpLidarA2")
         self.lidar.enable(self.sensorTime)
         self.lidar.enablePointCloud()
 
-        #Capteur de distance
+        # Distance sensors
         self.front_center_sensor = self.getDevice("front_center_sensor")
         self.side_left_sensor = self.getDevice("side_left_sensor")
         self.side_right_sensor = self.getDevice("side_right_sensor")
@@ -38,27 +38,43 @@ class VehicleDriver(Driver):
         self.side_left_sensor.enable(self.sensorTime)
         self.side_right_sensor.enable(self.sensorTime)
 
-        #Capteur de balise
+        # Checkpoint sensor
         self.up_sensor = self.getDevice("up_sensor")
         self.up_sensor.enable(self.sensorTime)
+
+        # Communication
+        self.receiver = self.getDevice("TT02_receiver")
+        print(f"{self.receiver=}")
+        self.receiver.enable(self.sensorTime)
+        self.receiver.setChannel(2 * self.i) # corresponds the the supervisor's emitter channel
+        self.emitter = self.getDevice("TT02_emitter")
+        print(f"{self.emitter=}")
+        self.emitter.setChannel(2 * self.i + 1) # corresponds the the supervisor's receiver channel
+
+        # Last data received from the supervisor (steering angle)
+        self.last_data = 0.
 
     #Vérification de l"état de la voiture
     def observe(self):
         try:
-            tableau = self.lidar.getRangeImage()
             #Division par 10 pour que la valeur soient entre 0 et 1
-            etat = np.array(tableau, dtype=np.float32)/10
+            return np.array(self.lidar.getRangeImage(), dtype=np.float32)/10
         except: #En cas de non retour lidar
             print("Pas de retour du lidar")
-            etat = np.zeros(self.lidar.getNumberOfPoints(), dtype=np.float32)
-
-        return etat
+            return np.zeros(self.lidar.getNumberOfPoints(), dtype=np.float32)
 
     #Fonction step de l"environnement GYM
     def step(self):
-        #steeringAngle = self.car.getField("steeringAngleInstruction").getSFFloat()
-        steeringAngle = 0
-        print("steeringAngle: ", steeringAngle)
+        # sends observation to the supervisor
+        self.emitter.send(self.observe().tobytes())
+
+        if self.receiver.getQueueLength() > 0:
+            while self.receiver.getQueueLength() > 1:
+                self.receiver.nextPacket()
+            self.last_data = np.frombuffer(self.receiver.getBytes(), dtype=np.float32)[0]
+
+        steeringAngle = self.last_data
+        print(f"steeringAngle({self.i}): ", steeringAngle)
         self.setSteeringAngle(steeringAngle)
         self.setCruisingSpeed(2)
 
