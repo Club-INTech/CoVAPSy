0b78909b623bfe8a357ff06662d008304ded8976
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-12-12 08:58:49 +0100

    Updated lidar doc and rearaged files

---
+++ Utils/Live_display_lidar.py
@@ -0,0 +1,15 @@
+import time
+from ..Lidar.HokuyoReader import HokuyoReader
+
+
+IP = '192.168.0.10'
+PORT = 10940
+
+if __name__ == '__main__':
+    sensor = HokuyoReader(IP, PORT)
+    sensor.stop()
+    # sensor.singleRead(0, 1080)
+    time.sleep(2)
+
+    sensor.startContinuous(0, 1080)
+    sensor.startPlotter()
