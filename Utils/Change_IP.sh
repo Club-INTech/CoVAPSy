4c435a7768c51e1f68470a6ed31cc5e5163719ec
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-12-13 13:15:22 +0100

    fixed misconfigerd Ips

--- Utils/Change_IP.sh
+++ Utils/Change_IP.sh
@@ -1,9 +1,9 @@
 sudo sh -c 'echo "[Match]" > /etc/systemd/network/eth0.network'
 sudo sh -c 'echo "Name=eth0" >> /etc/systemd/network/eth0.network'
 sudo sh -c 'echo "[Network]" >> /etc/systemd/network/eth0.network'
-sudo sh -c 'echo "Address=192.168.0.10/24" >> /etc/systemd/network/eth0.network'
-sudo sh -c 'echo "Gateway=192.168.0.20" >> /etc/systemd/network/eth0.network'
-sudo sh -c 'echo "DNS=192.168.0.20" >> /etc/systemd/network/eth0.network'
+sudo sh -c 'echo "Address=192.168.0.20/24" >> /etc/systemd/network/eth0.network'
+sudo sh -c 'echo "Gateway=192.168.0.1" >> /etc/systemd/network/eth0.network'
+sudo sh -c 'echo "DNS=192.168.0.1" >> /etc/systemd/network/eth0.network'
 sudo systemctl enable systemd-networkd
 sudo systemctl start systemd-networkd
 sudo systemctl restart systemd-networkd
