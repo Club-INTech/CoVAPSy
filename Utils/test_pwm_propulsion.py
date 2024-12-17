be28e2aae8a5cd739cbd5885a43573944de9cb72
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-12-17 10:54:43 +0100

    fix: adjust PWM parameters for propulsion control

--- Utils/test_pwm_propulsion.py
+++ Utils/test_pwm_propulsion.py
@@ -3,9 +3,9 @@ import time
 
 #paramètres de la fonction vitesse_m_s, à étalonner 
 direction_prop = 1 # -1 pour les variateurs inversés ou un petit rapport correspond à une marche avant
-pwm_stop_prop = 7.7
+pwm_stop_prop = 7.53
 point_mort_prop = 0.5
-delta_pwm_max_prop = 1. #pwm à laquelle on atteint la vitesse maximale
+delta_pwm_max_prop = 1.1 #pwm à laquelle on atteint la vitesse maximale
 
 vitesse_max_m_s_hard = 8 #vitesse que peut atteindre la voiture
 vitesse_max_m_s_soft = 2 #vitesse maximale que l'on souhaite atteindre
