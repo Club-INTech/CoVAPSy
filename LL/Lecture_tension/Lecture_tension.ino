b95d45bd5ec2fc36fd48ef4491abc842d90e276c
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-01-09 20:58:17 +0100

    Enhance battery voltage monitoring to support both LiPo and NiMh batteries

--- LL/Lecture_tension/Lecture_tension.ino
+++ LL/Lecture_tension/Lecture_tension.ino
@@ -1,9 +1,13 @@
 
 
-int sensorPin = A2;   // select the input pin for the battery sensor
-int r1 = 560;         // resistance of the first resistor
-int r2 = 1500;       // resistance of the second resistor
-float voltage = 0;   // variable to store the value read
+int sensorPin_Lipo = A2;   // select the input pin for the battery sensor
+int sensorPin_NiMh = A3;   // select the input pin for the battery sensor
+float r1_LiPo = 560;         // resistance of the first resistor
+float r1_NiMh = 560;       // resistance of the second resistor
+float r2_LiPo = 1500;       // resistance of the second resistor
+float r2_NiMh = 1000;     // resistance of the second resistor
+float voltage_LiPo = 0;   // variable to store the value read
+float voltage_NiMh = 0;   // variable to store the value read
 
 void setup() {
     Serial.begin(115200);
@@ -11,9 +15,16 @@ void setup() {
 
 void loop() {
     // read the value from the sensor:
-    voltage = analogRead(sensorPin);
-    voltage = voltage * (5.0 / 1023.0)*((r1+r2)/r1);
+    voltage_LiPo = analogRead(sensorPin_Lipo);
+    voltage_NiMh = analogRead(sensorPin_NiMh);
+    voltage_LiPo = voltage_LiPo * (5.0 / 1023.0) * ((r1_LiPo+r2_LiPo)/r1_LiPo);
+    voltage_NiMh = voltage_NiMh * (5.0 / 1023.0) * ((r1_NiMh+r2_NiMh)/r1_NiMh);
     // print the value to the serial port:
-    Serial.print("NiMh battery voltage: ");
-    Serial.println(voltage);
+    Serial.print("Lipo voltage: ");
+    Serial.print(voltage_LiPo);
+    Serial.print("  ");
+    Serial.print("NiMh voltage: ");
+    Serial.println(voltage_NiMh);
+
+    delay(100);
 }
