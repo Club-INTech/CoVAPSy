5c4faf40db0ca0fe071e3e9d0afdf4f92b6fe7d1
AntoFruch <antonin.fruchet@telecom-sudparis.eu>
2025-01-21 18:49:20 +0100

    Ebauche d'asservissment ( ca marche pas bien mais ca fait semblant )

--- Asservissement/Asserv2/Asserv2.ino
+++ Asservissement/Asserv2/Asserv2.ino
@@ -1,13 +1,19 @@
 #include <Servo.h>
+#include <EnableInterrupt.h>
+
+#define PIN_DIR 10
+#define PIN_MOT 9
+#define PIN_FOURCHE 14
+
 
 Servo moteur;
 Servo direction;
 
 
 //declaration des broches des differents composants
-const int pinMoteur=10;
-const int pinDirection=A0;
-const int pinFourche=3;
+const int pinMoteur=PIN_MOT;
+const int pinDirection=PIN_DIR;
+const int pinFourche=PIN_FOURCHE;
 
 //constantes utiles
 const int nb_trous=16*2; //nb de trous dans la roue qui tourne devant la fourche optique
@@ -15,12 +21,12 @@ const int distanceUnTour=79; //distance parcourue par la voiture apres un tour d
 
 //variables
 char command;
-float Vcons=1000; //consigne
+float Vcons=0; //consigne
 float vitesse=0; //vitesse de la voiture
 float ecart=0;
 float vieuxEcart=0;
 float vieuxTemps=0; //variable utilisee pour mesurer le temps qui passe
-float Kp=0.05; //correction prop
+double Kp=0.05; //correction prop
 float Ki=0; //correction integrale
 float Kd=0.; //correction derivee
 float integral=0;//valeur de l'integrale dans le PID
@@ -29,33 +35,68 @@ volatile int count=0; //variable utilisee pour compter le nombre de fronts monta
 volatile int vieuxCount=0; //stocke l'ancienne valeur de count pour ensuite faire la difference
 volatile byte state=LOW;
 
+float mesures[10]; // tableau de mesures pour lisser
+
+float getMeanSpeed(float dt){
+  int length = sizeof(mesures)/sizeof(mesures[0]);
+  //ajout d'une nouvelle mesure et suppression de l'ancienne
+  for (int i=length-1;i>0;i--){
+      mesures[i]=mesures[i-1];
+  }
+  mesures[0] = getSpeed(dt);
+
+  //Calcul d'une moyenne pour lisser les mesures qui sont trop dipersées sinon
+  float sum=0;
+  for (int i=0;i<length;i++){
+    sum+=mesures[i];
+  }
+
+  #if 0
+  for(int i=0;i<length;i++){
+    Serial.print(mesures[i]);
+    Serial.print(" , ");
+  }
+  Serial.println(sum/length);
+  #endif
 
 
+  return sum/length;
+}
 
 
-float getSpeed(float dt){
+float getSpeed(float dt){  
   int N = count - vieuxCount; //nombre de fronts montant et descendands après chaque loop
   float V = ((float)N/(float)nb_trous)*distanceUnTour/(dt*1e-3); //16 increments -> 1 tour de la roue et 1 tour de roue = 79 mm 
   vieuxCount=count;
   vieuxTemps=millis();
+  /*Serial.print(count+" ");
+  Serial.print(vieuxCount+" ");
+  Serial.print(N+" ");
+  Serial.println((int)V+" ");*/
   return V;
 }
 
 void blink(){ //on compte tous les fronts
+  //Serial.print(count);
   count++;
 }
 
 
-float PID(float cons,float V,float dt){
-  float e = cons - V;
+float PID(float cons,float mes,float dt){
+  float e = cons - mes;
   float P = Kp*e;
+  
+  #if 0
   integral=constrain(integral + e*dt,-500,500); //on sature pour que l'integrale ne diverge pas
   float I= Ki*integral;
+  
   derivee =(e-vieuxEcart)/dt;
   vieuxEcart=e;
   float D=Kd*derivee;
-  return P + I + D;
+  #endif
+  return P ;//+ I + D;
 }
+
 
 void setup() {
   Serial.begin(115200);
@@ -65,47 +106,48 @@ void setup() {
 
   pinMode(pinDirection,OUTPUT);
   direction.attach(pinDirection,0,2000);
+  
   pinMode(pinFourche,INPUT_PULLUP);
+  enableInterrupt(PIN_FOURCHE, blink, CHANGE); //on regarde a chaque fois que le signal de la fourche change (Montants et Descendants)
 
   moteur.writeMicroseconds(1500);
   delay(2000);
+  moteur.writeMicroseconds(1590);
+
 
   delay(10);
-  Serial.println("test");
-
-  attachInterrupt(1,blink,CHANGE); //on regarde a chaque fois que le signal de la fourhce change (Montants et Descendants)
-
+  Serial.println("init");
 }
-
-
-
 
 void loop() {
   
   command=Serial.read();
   switch (command){ //pour regler Vcons
     case 'a':
-    Vcons+=100;
+    Vcons+=50;
     break;
     case 'z':
-    Vcons-=100;
+    Vcons-=50;
     break;
     case 'q':
     Vcons=0;
     break;
   }
   int deltaT = millis()-vieuxTemps; //temps qui est passé pendant un loop (en millisecondes)
-  vitesse=getSpeed(deltaT);
+  vitesse=getMeanSpeed(deltaT);
 
-  int out = constrain(PID(Vcons,vitesse,deltaT) + 1500, 1500, 1650);
-  moteur.writeMicroseconds(out);
+  int out = PID(Vcons,vitesse,deltaT);
+  //moteur.writeMicroseconds(Vcons+1500);  
+  moteur.writeMicroseconds(1500 + Vcons/10 + out);
 
+  #if 1
   Serial.print("");
   Serial.print(Vcons);
   Serial.print(", ");
-  Serial.print("");
   Serial.print(vitesse);
+  Serial.print(", ");
   Serial.print("out=");
   Serial.println(out);
-  delay(5);
+  #endif
+  delay(10);
 }
