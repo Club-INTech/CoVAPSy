0e6dc5c5a31e165244ffac12b37899297afcb6e5
AntoFruch <antonin.fruchet@telecom-sudparis.eu>
2025-01-29 22:28:33 +0100

    Asservissement fonctionel ( pas de compatiobilité avec une com port serie avec le RPI)

--- Asservissement/Asserv2/Asserv2.ino
+++ Asservissement/Asserv2/Asserv2.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include <EnableInterrupt.h>
 
+
 #define PIN_DIR 10
 #define PIN_MOT 9
 #define PIN_FOURCHE 14
@@ -8,6 +9,7 @@
 
 Servo moteur;
 Servo direction;
+
 
 
 //declaration des broches des differents composants
@@ -23,18 +25,21 @@ const int distanceUnTour=79; //distance parcourue par la voiture apres un tour d
 char command;
 float Vcons=0; //consigne
 float vitesse=0; //vitesse de la voiture
+
+//PID
 float ecart=0;
 float vieuxEcart=0;
 float vieuxTemps=0; //variable utilisee pour mesurer le temps qui passe
-double Kp=0.05; //correction prop
-float Ki=0; //correction integrale
+float Kp=0.05; //correction prop
+float Ki=0.1; //correction integrale
 float Kd=0.; //correction derivee
 float integral=0;//valeur de l'integrale dans le PID
 float derivee=0; //valeur de la derivee dans le PID
+
+//mesures
 volatile int count=0; //variable utilisee pour compter le nombre de fronts montants/descendants
 volatile int vieuxCount=0; //stocke l'ancienne valeur de count pour ensuite faire la difference
 volatile byte state=LOW;
-
 float mesures[10]; // tableau de mesures pour lisser
 
 float getMeanSpeed(float dt){
@@ -62,8 +67,6 @@ float getMeanSpeed(float dt){
 
   return sum/length;
 }
-
-
 float getSpeed(float dt){  
   int N = count - vieuxCount; //nombre de fronts montant et descendands après chaque loop
   float V = ((float)N/(float)nb_trous)*distanceUnTour/(dt*1e-3); //16 increments -> 1 tour de la roue et 1 tour de roue = 79 mm 
@@ -75,29 +78,29 @@ float getSpeed(float dt){
   Serial.println((int)V+" ");*/
   return V;
 }
-
 void blink(){ //on compte tous les fronts
   //Serial.print(count);
   count++;
 }
-
-
 float PID(float cons,float mes,float dt){
   float e = cons - mes;
+  
+  //Terme Proportionel
   float P = Kp*e;
   
-  #if 0
-  integral=constrain(integral + e*dt,-500,500); //on sature pour que l'integrale ne diverge pas
+  // Terme Intégral
+  integral=integral + e*dt;
   float I= Ki*integral;
   
+  #if 0
+  //terme dérivé
   derivee =(e-vieuxEcart)/dt;
   vieuxEcart=e;
   float D=Kd*derivee;
   #endif
-  return P ;//+ I + D;
+
+  return P + I;
 }
-
-
 void setup() {
   Serial.begin(115200);
 
@@ -118,13 +121,12 @@ void setup() {
   delay(10);
   Serial.println("init");
 }
-
 void loop() {
-  
+  // Commandes pour debugger
   command=Serial.read();
-  switch (command){ //pour regler Vcons
+  switch (command){ //pour regler les parametres
     case 'a':
-    Vcons+=50;
+    Vcons+=200;
     break;
     case 'z':
     Vcons-=50;
@@ -132,19 +134,35 @@ void loop() {
     case 'q':
     Vcons=0;
     break;
+    case 'i':
+    Ki+=0.01;
+    break;
+    case 'k':
+    Ki-=0.01;
+    break;
+    case 'u':
+    Kp+=0.01;
+    break;
+    case 'j':
+    Kp-=0.01;
+    break;
   }
   int deltaT = millis()-vieuxTemps; //temps qui est passé pendant un loop (en millisecondes)
-  vitesse=getMeanSpeed(deltaT);
+  vitesse=getMeanSpeed(deltaT); // on recup la vitesse lissée
 
-  int out = PID(Vcons,vitesse,deltaT);
-  //moteur.writeMicroseconds(Vcons+1500);  
-  moteur.writeMicroseconds(1500 + Vcons/10 + out);
+  int out = PID(Vcons,vitesse,float(deltaT)/1e3);
+  moteur.writeMicroseconds(constrain(1500 + out,1500,2000));
 
+  //print debug
   #if 1
   Serial.print("");
   Serial.print(Vcons);
   Serial.print(", ");
   Serial.print(vitesse);
+  Serial.print(", ");
+  Serial.print(Ki);
+  Serial.print(", ");
+  Serial.print(Kp);
   Serial.print(", ");
   Serial.print("out=");
   Serial.println(out);
