6cd6e0aa8fdf4479f12c15c369c27d11c01b1d74
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-01-29 20:33:08 +0100

    fix: update PWM angle parameters for improved control in Simple.py and test_pwm_direction.py

--- Simple.py
+++ Simple.py
@@ -17,10 +17,10 @@ vitesse_max_m_s_soft = 2 #vitesse maximale que l'on souhaite atteindre
 
 
 #paramètres de la fonction set_direction_degre
-direction = 1 #1 pour angle_pwm_min a gauche, -1 pour angle_pwm_min à droite
-angle_pwm_min = 6   #min
-angle_pwm_max = 9   #max
-angle_pwm_centre= 7.5
+direction = -1 #1 pour angle_pwm_min a gauche, -1 pour angle_pwm_min à droite
+angle_pwm_min = 6.91 #min
+angle_pwm_max = 10.7   #max
+angle_pwm_centre= 8.805
 
 angle_degre_max = +18 #vers la gauche
 angle_degre=0
