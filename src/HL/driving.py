4f77233a65876ce0b4546e3bf1c1c9c933e6729c
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-03-06 16:30:11 +0100

    fix: adjust reverse speed and delay in driving logic

--- src/HL/driving.py
+++ src/HL/driving.py
@@ -93,8 +93,8 @@ class Car():
         time.sleep(0.2)
         self.set_vitesse_m_s(0)
         time.sleep(0.2)
-        self.set_vitesse_m_s(-1.5)
-        time.sleep(0.5)
+        self.set_vitesse_m_s(-2)
+        time.sleep(1)
 
     def set_direction_degre(self, angle_degre):
         angle_pwm = self.angle_pwm_centre + self.direction * \
