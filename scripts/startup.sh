07a23fd5c2bae94ecf569f8e390261003d679556
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-03-11 18:53:45 +0100

    Add WiFi connection check and log outputs in startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,14 +3,17 @@
 # Specify the PATH
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
+# Wait until WiFi is acquired
+while ! ping -c 1 google.com &> /dev/null; do
+    echo "Waiting for WiFi connection..."
+    sleep 5
+done
+
 # Change to the project directory
 cd /home/intech/CoVAPSy
 
 # Pull the latest changes from the repository
-git pull 
-
-# Create the test directory
-mkdir -p /home/intech/CoVAPSy/test
+git pull >> /home/intech/CoVAPSy/git_pull.log 2>&1
 
 # Run the main Python script
-python /home/intech/CoVAPSy/src/HL/main.py
+python /home/intech/CoVAPSy/src/HL/main.py >> /home/intech/CoVAPSy/main.log 2>&1
