2a0f21ff533a8f271bf4c468a5e89be6930368f6
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2025-03-11 18:17:13 +0100

    Remove logging from git pull and main script execution in startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,10 +7,10 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 cd /home/intech/CoVAPSy
 
 # Pull the latest changes from the repository
-git pull >> /home/intech/CoVAPSy/git_pull.log 2>&1
+git pull 
 
 # Create the test directory
 mkdir -p /home/intech/CoVAPSy/test
 
 # Run the main Python script
-python /home/intech/CoVAPSy/src/HL/main.py >> /home/intech/CoVAPSy/main.log 2>&1
+python /home/intech/CoVAPSy/src/HL/main.py
