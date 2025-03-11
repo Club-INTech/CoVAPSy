8c359fd7bc564e53db532c02c0706b590c1035c4
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2025-03-11 12:57:37 +0100

    ???

--- Simulateur/config.py
+++ Simulateur/config.py
@@ -1,7 +1,7 @@
 # just a file that lets us define some constants that are used in multiple files the simulation
 from torch.cuda import is_available
 
-n_simulations = 8
+n_simulations = 1
 n_vehicles = 4
 n_stupid_vehicles = 2
 n_actions_steering = 16
