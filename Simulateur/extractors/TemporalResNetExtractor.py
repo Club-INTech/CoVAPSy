0322913d892ecbba330234c15739f1102934c0bd
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2025-03-11 09:44:43 +0100

    ???

--- Simulateur/extractors/TemporalResNetExtractor.py
+++ Simulateur/extractors/TemporalResNetExtractor.py
@@ -31,10 +31,10 @@ class Compressor(nn.Module):
     def __init__(self, device: str = "cpu"):
         super().__init__()
         # WARNING : do not use inplace=True because it would modify the rollout buffer
-        self.input_dropout = ChannelDependentDropout2d([0.001, 0.999])
+        self.input_dropout = ChannelDependentDropout2d([0.001, 0.35])
         self.conv = nn.Conv2d(2, 64, kernel_size=7, stride=2, padding=3, device=device)
         self.bn = nn.BatchNorm2d(64, device=device)
-        self.relu = nn.ReLU(inplace=False)
+        self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout2d(0.3, inplace=False)
         self.dropout = nn.Dropout2d(0.3, inplace=False)
         self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
@@ -70,7 +70,7 @@ class ResidualBlock(nn.Module):
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, device=device)
         self.bn1 = nn.BatchNorm2d(out_channels, device=device)
         self.bn2 = nn.BatchNorm2d(out_channels, device=device)
-        self.relu = nn.ReLU(inplace=False)
+        self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout2d(0.3, inplace=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
